2f432f21fb25a9fd5e326a7c3e5aebd32dd2c2a9
Vincent Tran <vrtlal09@gmail.com>
2026-06-15 12:41:07 -0700

    hello

---
+++ datatypes.py
@@ -0,0 +1,3 @@
+movies = ["Pacific Rim", "Forrest Gump", "Iron Man", "Spider-Man", "Prison Break"]
+
+favoriteThings = {"Drink": "Water", "Hobby": "Gaming", "Color": "Blue", "Day": "Saturday"}
---
+++ variables.py
@@ -0,0 +1,5 @@
+first_name = "Vincent"
+last_name = "Tran"
+age = 5
+
+print(age)
