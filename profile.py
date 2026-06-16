dafbf6362689abb0f371b8ba07e312febe6ac45f
Vincent Tran <vrtlal09@gmail.com>
2026-06-16 16:44:40 -0700

    OK

---
+++ profile.py
@@ -0,0 +1,10 @@
+full_name = "Vincent Tran"
+nickname = "Vince"
+age = 17
+used_python_before  = false
+hobbies = ["gaming", "hanging out with friends and family", "movies"]
+favorites = {
+    "Drink": "water",
+    "Color": "blue",
+    "Day": "saturday"
+}
